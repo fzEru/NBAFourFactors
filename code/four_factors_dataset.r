5ae9cb163698983b831900386090750d60f5bbf2
M F K <72181142+mfaizk17@users.noreply.github.com>
2021-02-24 00:10:35 -0500

    add comments to code

--- code/four_factors_dataset.r
+++ code/four_factors_dataset.r
@@ -1,8 +1,13 @@
+# install libraries
 library(readr)
 library(dplyr)
 
+# read in original dataset
 NBA <- read_csv("data/NBAadv2020.csv")
 
+# filter only columns from the original dataset that are needed for analysis
 NBAFF <- NBA %>%
   select(Rk, Player, Pos, 'TS%', FTr, 'ORB%', 'DRB%', 'TRB%', 'TOV%', WS, VORP)
+
+# view new dataframe
 View(NBAFF)
